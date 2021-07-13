4dd0afd9a6db3344a8152d1010aae9278746eed7
Ollie Nichols <ollienichols22@gmail.com>
2021-07-13 09:08:36 +0000

    Improved formatting

--- programs/questions.py
+++ programs/questions.py
@@ -1,58 +1,57 @@
-    # INSTRUCTIONS
+# INSTRUCTIONS
 
-    # In case it is not clear, the Question appears first, then examples, then any hints and finally the function that you need to complete appears underneath:
+# In case it is not clear, the Question appears first, then examples, then any hints and finally the function that you need to complete appears underneath:
 
-    # <QUESTION>
+# <QUESTION>
 
-    # <EXAMPLES>
+# <EXAMPLES>
 
-    # <HINT>
+# <HINT>
 
-    # You are allowed access to the internet for this assessment, or you could use the DOCUMENTATION that comes bundled with your Python installation.  You should already be comfortable accessing this documentation, but to summarise:
+# You are allowed access to the internet for this assessment, or you could use the DOCUMENTATION that comes bundled with your Python installation.  You should already be comfortable accessing this documentation, but to summarise:
 
-    # Access Python from you CLI
+# Access Python from you CLI
 
-    # Type help() or for example help(str)
+# Type help() or for example help(str)
 
 
+# <QUESTION 1>
 
-    # <QUESTION 1>
+# Define a function that can accept two strings as input and returns the string with maximum length to the console.
 
-    # Define a function that can accept two strings as input and returns the string with maximum length to the console. 
+# If two strings have the same length, then the function should return both strings separated by a " ".
 
-    # If two strings have the same length, then the function should return both strings separated by a " ".
+# In this case, the strings should be returned in the same order in which they were given.
 
-    # In this case, the strings should be returned in the same order in which they were given.
+# <EXAMPLES>
 
-    # <EXAMPLES>
+# one("hi","hello") → "hello"
+# one("three", "two") → "three"
+# one("three", "hello") → "three hello"
 
-    # one("hi","hello") → "hello"
-    # one("three", "two") → "three"
-    # one("three", "hello") → "three hello"
+# <HINT>
 
-    # <HINT>
+# What was the name of the function we have seen to check the length of a container?  Use your CLI to access the Python documentation and get help(len).
 
-    # What was the name of the function we have seen to check the length of a container?  Use your CLI to access the Python documentation and get help(len).
 
 def one(input1, input2):
     if len(input1) == len(input2):
         return f"{input1} {input2}"
 
     else:
-        return max(input1, input2, key=len) 
+        return max(input1, input2, key=len)
 
-        # equivalently we could do 
+        # equivalently we could do
         # if len(input1) > len(input2):
         #   return input1
         # else:
         #   return input2
 
-
     # <QUESTION 2>
 
     # Return the string that is between the first and last appearance of "bert" in the given string
 
-    # Return the empty string "" if there is not 2 occurances of "bert" 
+    # Return the empty string "" if there is not 2 occurances of "bert"
 
     # IGNORE CASE
 
@@ -70,20 +69,20 @@ def one(input1, input2):
 
     # Use your CLI to access the Python documentation and get help manipulating strings - help(str).
 
+
 def two(input):
     first_bert = input.casefold().find("bert")
-    second_bert = input.casefold().find("bert", first_bert+4)
+    second_bert = input.casefold().find("bert", first_bert + 4)
     # second argument in str.find allows us to specify where we start looking
 
     if first_bert != -1 and second_bert != -1:
         # if not found, the result of str.find will be -1
-        return input[first_bert+4 : second_bert]  
-    
+        return input[first_bert + 4 : second_bert]
+
     else:
         return ""
-        # need to return a blank string, can't return nothing! 
+        # need to return a blank string, can't return nothing!
         # we know this from the examples
-
 
     # <QUESTION 3>
 
@@ -92,7 +91,7 @@ def two(input):
     # if this number is divisible by 5 return "buzz"
     # if this number is divisible by both 3 and 5 return "fizzbuzz"
     # if this number is not divisible by 3 or 5 return "null"
-        
+
     # <EXAMPLES>
 
     # three(3) → "fizz"
@@ -104,15 +103,17 @@ def two(input):
 
     # No Hints for this question
 
+
 def three(arg1):
     result = ""
 
-    if arg1 % 3 == 0: result += "fizz"
-    if arg1 % 5 == 0: result += "buzz"
+    if arg1 % 3 == 0:
+        result += "fizz"
+    if arg1 % 5 == 0:
+        result += "buzz"
 
     return result or "null"
     # will return "null" if result is a blank string
-
 
     # <QUESTION 4>
 
@@ -125,7 +126,7 @@ def three(arg1):
     # "86" will = the integer 14
 
     # You then need to return the highest value, in the example above this would be 14.
-        
+
     # <EXAMPLES>
 
     # four("55 72 86") → 14
@@ -136,10 +137,11 @@ def three(arg1):
 
     # help(int) for working with numbers and help(str) for working with Strings.
 
+
 def four(arg1):
     max_value = 0
     for num in map(int, arg1.split()):
-        # map(int, ...) will convert a string of numbers back 
+        # map(int, ...) will convert a string of numbers back
         # into integers! very handy to know
 
         value = sum(map(int, str(num)))
@@ -150,8 +152,6 @@ def four(arg1):
             max_value = value
 
     return max_value
-
-
 
     # <QUESTION 5>
 
@@ -178,22 +178,21 @@ def four(arg1):
 
     # help(str) and help(list), you might also need to use a function that can create a list of numbers for you, try help(range).
 
+
 def five(input):
     result = []
-    words = input.split(',')  # get list of words
+    words = input.split(",")  # get list of words
 
     for i in range(0, len(words), 4):
         # third argument in range is the step, so we go up by 4 at a time
 
-        name = words[i]         # first argument is the name we want
-        encrypted = words[i+2]  # third argument is whether or not it's encrypted
+        name = words[i]  # first argument is the name we want
+        encrypted = words[i + 2]  # third argument is whether or not it's encrypted
 
         if encrypted == "False" and name not in result:
             result.append(name)
 
     return result
-
-
 
     # <QUESTION 6>
 
@@ -212,14 +211,14 @@ def five(input):
 
     # Step through the logic here in order to solve the problem, you may find help(range) helpful.
 
+
 def six(input):
     return "cei" in input or ("ie" in input and "cie" not in input)
     # not really robust but it passes the tests
 
-
     # <QUESTION 7>
 
-    # Write a function which returns the integer number of vowels in a given string. 
+    # Write a function which returns the integer number of vowels in a given string.
     # You should ignore case.
 
     # <EXAMPLES>
@@ -231,21 +230,21 @@ def six(input):
 
     # How do we ignore case in a String? help(str) may offer some insight.
 
+
 def seven(input):
     vowel_count = 0
 
-    for vowel in 'a', 'e', 'i', 'o', 'u':
+    for vowel in "a", "e", "i", "o", "u":
 
         vowel_count += input.casefold().count(vowel)
         # casefold used to ignore case, as specified in question
 
     return vowel_count
 
-
     # <QUESTION 8>
 
     # Write a function which takes an input (between 1 and 10 inclusive) and multiplies it by all the numbers before it.
-    # eg If the input is 4, the function calculates 4x3x2x1 = 24 
+    # eg If the input is 4, the function calculates 4x3x2x1 = 24
 
     # <EXAMPLES>
 
@@ -257,10 +256,11 @@ def seven(input):
 
     # You may need to create a list of numbers from 0 to i, take a look at help(range).
 
+
 def eight(input):
     result = 1
 
-    for x in range(1, input+1):
+    for x in range(1, input + 1):
         # make sure we include the given number, so range should
         # stop at input+1
 
@@ -268,7 +268,6 @@ def eight(input):
         # equivalent to result = result * x
 
     return result
-
 
     # <QUESTION 9>
 
@@ -288,8 +287,9 @@ def eight(input):
 
     # Take a look at the documentation for Strings, List and range.
 
+
 def nine(inputString, char):
-    inputString = inputString.replace(" ","")
+    inputString = inputString.replace(" ", "")
     actual_index = inputString.find(char)
 
     if actual_index != -1:
@@ -300,13 +300,12 @@ def nine(inputString, char):
     else:
         return -1
 
-
     # <QUESTION 10>
 
-    # Given a string, int and a char, return a boolean value if the 'nth' 
+    # Given a string, int and a char, return a boolean value if the 'nth'
     # (represented by the int provided) char of the String supplied is the same as the char supplied.
     # The int provided will NOT always be less than than the length of the String.
-    # IGNORE case and Whitespace. 
+    # IGNORE case and Whitespace.
 
     # <EXAMPLES>
 
@@ -318,11 +317,12 @@ def nine(inputString, char):
 
     # How do we find the length of a container, take a look at help(len), you will also need to look at help(str) for String manipulation.
 
-def ten(string, int, char):
-    string = string.replace(" ","").casefold()
 
-    if int < len(string): 
-        return string[int-1] == char
-    
+def ten(string, int, char):
+    string = string.replace(" ", "").casefold()
+
+    if int < len(string):
+        return string[int - 1] == char
+
     else:
         return False
--- tests/assessment_test.py
+++ tests/assessment_test.py
@@ -1,11 +1,13 @@
 from programs import questions
 
+
 def test_one():
-    assert questions.one("hi","hello") == "hello"
+    assert questions.one("hi", "hello") == "hello"
     assert questions.one("three", "two") == "three"
     assert questions.one("three", "hello") == "three hello"
     assert questions.one("echo", "print") == "print"
-    assert questions.one("fire","rib") == "fire"
+    assert questions.one("fire", "rib") == "fire"
+
 
 def test_two():
     assert questions.two("bertclivebert") == "clive"
@@ -14,12 +16,14 @@ def test_two():
     assert questions.two("xxbertyy") == ""
     assert questions.two("xxbeRTyy") == ""
 
+
 def test_three():
     assert questions.three(3) == "fizz"
     assert questions.three(10) == "buzz"
     assert questions.three(15) == "fizzbuzz"
     assert questions.three(8) == "null"
     assert questions.three(75) == "fizzbuzz"
+
 
 def test_four():
     assert questions.four("55 72 86") == 14
@@ -28,12 +32,25 @@ def test_four():
     assert questions.four("98 63 34 1 13") == 17
     assert questions.four("98 107 415") == 17
 
+
 def test_five():
     assert questions.five("Jeff,random.py,False,1445") == ["Jeff"]
-    assert questions.five("Bert,numberGen.py,True,1447,Bert,integers.py,True,1318,Jeff,floats.py,False,1445") == ["Jeff"]
-    assert questions.five("Bert,boolean.py,False,1447,Bert,conditions.py,False,1318,Jeff,loops.py,False,1445") == ["Bert","Jeff"]
-    assert questions.five("Bert,prime.py,True,1447,Bert,ISBN.py,False,1318,Jeff,OOP.py,False,1445") == ["Bert","Jeff"]
-    assert questions.five("Bert,files.py,True,1447,Bert,tests.py,True,1318,Jeff,app.py,True,1445") == []
+    assert questions.five(
+        "Bert,numberGen.py,True,1447,Bert,integers.py,True,1318,Jeff,floats.py,False,1445"
+    ) == ["Jeff"]
+    assert questions.five(
+        "Bert,boolean.py,False,1447,Bert,conditions.py,False,1318,Jeff,loops.py,False,1445"
+    ) == ["Bert", "Jeff"]
+    assert questions.five(
+        "Bert,prime.py,True,1447,Bert,ISBN.py,False,1318,Jeff,OOP.py,False,1445"
+    ) == ["Bert", "Jeff"]
+    assert (
+        questions.five(
+            "Bert,files.py,True,1447,Bert,tests.py,True,1318,Jeff,app.py,True,1445"
+        )
+        == []
+    )
+
 
 def test_six():
     assert questions.six("ceiling") == True
@@ -42,12 +59,14 @@ def test_six():
     assert questions.six("height") == False
     assert questions.six("receive") == True
 
+
 def test_seven():
-    assert questions.seven("Hello") == 2 
+    assert questions.seven("Hello") == 2
     assert questions.seven("hEelLoooO") == 6
     assert questions.seven("WhitEboarD") == 4
     assert questions.seven("as") == 1
     assert questions.seven("pass") == 1
+
 
 def test_eight():
     assert questions.eight(1) == 1
@@ -56,16 +75,18 @@ def test_eight():
     assert questions.eight(6) == 720
     assert questions.eight(8) == 40320
 
+
 def test_nine():
-    assert questions.nine("This is a Sentence","s") == 4
-    assert questions.nine("This is a Sentence","S") == 8
-    assert questions.nine("Fridge for sale","z") == -1
+    assert questions.nine("This is a Sentence", "s") == 4
+    assert questions.nine("This is a Sentence", "S") == 8
+    assert questions.nine("Fridge for sale", "z") == -1
     assert questions.nine("I love Python", "L") == -1
     assert questions.nine("I LOVE PYTHON", "L") == 2
 
+
 def test_ten():
-    assert questions.ten("The",2,"h") == True
-    assert questions.ten("AAbb",1,"b") == False
-    assert questions.ten("Hi-There",10,"e") == False
-    assert questions.ten("HEY",2,"e") == True
-    assert questions.ten("on-premise",3,"-") == True
+    assert questions.ten("The", 2, "h") == True
+    assert questions.ten("AAbb", 1, "b") == False
+    assert questions.ten("Hi-There", 10, "e") == False
+    assert questions.ten("HEY", 2, "e") == True
+    assert questions.ten("on-premise", 3, "-") == True
